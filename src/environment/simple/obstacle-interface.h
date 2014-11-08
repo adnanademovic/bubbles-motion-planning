96e968a0e0db70c5a0c8c59195f08ad0ebc62552
Adnan Ademovic <adnanademovic100@gmail.com>
2014-11-08 01:16:27 +0100

    Adding environment interfaces.
    + Adding starting elements of robot's interface.
    + Adding interface for obstacles in the simple wireframe environment.

---
+++ src/environment/simple/obstacle-interface.h
@@ -0,0 +1,53 @@
+//
+// Copyright (c) 2014, Adnan Ademovic
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#ifndef COM_ADEMOVIC_BUBBLESMP_ENVIRONMENT_SIMPLE_OBSTACLE_INTERFACE_H_
+#define COM_ADEMOVIC_BUBBLESMP_ENVIRONMENT_SIMPLE_OBSTACLE_INTERFACE_H_
+
+namespace com {
+namespace ademovic {
+namespace bubblesmp {
+namespace environment {
+namespace simple {
+
+// Defines interface for obstacles of wireframe robots.
+class ObstacleInterface {
+ public:
+  virtual ~ObstacleInterface() {};
+  virtual double DistanceToLine(double x1, double y1, double z1,
+                                double x2, double y2, double z2) const = 0;
+
+ protected:
+  ObstacleInterface() {};
+};
+
+}  // namespace simple
+}  // namespace environment
+}  // namespace bubblesmp
+}  // namespace ademovic
+}  // namespace com
+
+#endif  // COM_ADEMOVIC_BUBBLESMP_ENVIRONMENT_SIMPLE_OBSTACLE_INTERFACE_H_
