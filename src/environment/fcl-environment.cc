8dc8a3d844006048935d9265e09ce8fb542122f5
Adnan Ademovic <adnanademovic100@gmail.com>
2015-03-27 09:48:08 +0100

    Add FCL port for environment.

---
+++ src/environment/fcl-environment.cc
@@ -0,0 +1,281 @@
+//
+// Copyright (c) 2015, Adnan Ademovic
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#include "fcl-environment.h"
+
+#include <algorithm>
+#include <cmath>
+
+#include <fcl/BVH/BVH_model.h>
+#include <fcl/distance.h>
+
+namespace com {
+namespace ademovic {
+namespace bubblesmp {
+namespace environment {
+namespace {
+
+double PointDistanceToAxis(const fcl::Vec3f& point, const fcl::Vec3f& axis) {
+  double l = point.dot(axis);
+  return (point - axis * l).length();
+}
+
+double PointDistanceToVector(const fcl::Vec3f& point, const fcl::Vec3f& vect) {
+  double point_a_distance(point.sqrLength());
+  fcl::Vec3f d = point - vect;
+  double point_b_distance(d.sqrLength());
+  fcl::Vec3f u = -vect;
+  u.normalize();
+  if (point_a_distance < point_b_distance) {
+    d = point;
+    d.normalize();
+    double c = d.dot(fcl::Vec3f(vect).normalize());
+    return (c > 0.0)
+        ? sqrt(point_a_distance * (1.0 - c * c))
+        : sqrt(point_a_distance);
+  } else {
+    d.normalize();
+    double c = d.dot((-vect).normalize());
+    return (c > 0.0)
+        ? sqrt(point_b_distance * (1.0 - c * c))
+        : sqrt(point_b_distance);
+  }
+}
+
+// TODO: Make parsing more sophisticated and do other parsing besides bin STL.
+// TODO: Fix parsing for big endian machines.
+fcl::BVHModel<fcl::OBBRSS>* ParseModel(
+    const std::string& filename, fcl::Transform3f* t,
+    double x, double y, double z, double* l, double* r) {
+  FILE* f = fopen(filename.c_str(), "rb");
+  uint8_t header[80];
+  uint32_t triangle_count;
+  fread(header, sizeof(header[0]), 80, f);
+  fread(&triangle_count, sizeof(triangle_count), 1, f);
+  float input[3][3];
+  fcl::Vec3f p[3];
+  float normal[3];
+  uint16_t attribute;
+  fcl::BVHModel<fcl::OBBRSS>* model = new fcl::BVHModel<fcl::OBBRSS>;
+  model->beginModel();
+  *l = 0.0;
+  *r = 0.0;
+  for (uint32_t triangle = 0; triangle < triangle_count; ++triangle) {
+    fread(normal, sizeof(normal[0]), 3, f);
+    fread(input[0], sizeof(input[0][0]), 3, f);
+    fread(input[1], sizeof(input[1][0]), 3, f);
+    fread(input[2], sizeof(input[2][0]), 3, f);
+    fread(&attribute, sizeof(attribute), 1, f);
+    for (int tri = 0; tri < 3; ++tri) {
+      p[tri] = t->transform(fcl::Vec3f(
+          input[tri][0], input[tri][1], input[tri][2]));
+      *l = std::max(*l, p[tri].dot({x, y, z}));
+      *r = std::max(*r, PointDistanceToVector(
+          p[tri], {x * (*l), y * (*l), z * (*l)}));
+    }
+    model->addTriangle(p[0], p[1], p[2]);
+  }
+  model->endModel();
+  fclose(f);
+  return model;
+}
+
+}  // namespace
+
+FclEnvironment::FclEnvironment(const std::string& configuration) {
+  boost::filesystem::path config_file_path(configuration);
+  config_file_path.remove_filename();
+  boost::property_tree::ptree config_tree;
+  boost::property_tree::read_json(configuration, config_tree);
+  ConfigureFromPtree(config_tree, config_file_path);
+}
+
+FclEnvironment::FclEnvironment(
+    const boost::property_tree::ptree& config_tree,
+    const boost::filesystem::path& config_file_path) {
+  ConfigureFromPtree(config_tree, config_file_path);
+}
+
+bool FclEnvironment::IsCollision(const std::vector<double>& q) const {
+  std::lock_guard<std::mutex> guard(guard_mutex_);
+  fcl::Transform3f pose;
+  fcl::Matrix3f joint_angle_rotation;
+  int segment = -1;
+  for (int part = 0; part < part_count_; ++part) {
+    if (is_joint_start_[part]) {
+      if (segment > -1)
+        pose *= *dh_parameters_[segment];
+      ++segment;
+      joint_angle_rotation.setEulerZYX(0.0, 0.0, q[segment]);
+      pose *= fcl::Transform3f(joint_angle_rotation);
+    }
+    parts_[part]->setTransform(pose);
+
+    fcl::CollisionRequest request;
+    fcl::CollisionResult result;
+    fcl::collide(parts_[part].get(), environment_.get(), request, result);
+    if (result.isCollision())
+      return true;
+  }
+  return false;
+}
+
+EnvironmentInterface::DistanceProfile FclEnvironment::GetDistanceProfile(
+    const std::vector<double>& q) const {
+  std::lock_guard<std::mutex> guard(guard_mutex_);
+  fcl::Transform3f pose;
+  fcl::Matrix3f joint_angle_rotation;
+  std::vector<fcl::Vec3f> ax_P; // stores all joint positions
+  std::vector<fcl::Vec3f> ax_O; // stores all axis orientations
+  EnvironmentInterface::DistanceProfile distances;
+  int segment = -1;
+
+  fcl::Vec3f p, p_prev;
+  fcl::Matrix3f R;
+  for (int part = 0; part < part_count_; ++part) {
+    if (is_joint_start_[part]) {
+      if (segment > -1)
+        pose *= *dh_parameters_[segment];
+      ++segment;
+      ax_P.push_back(pose.getTranslation());
+      R = pose.getRotation();
+      ax_O.push_back({R(0, 2), R(1, 2), R(2, 2)});
+      joint_angle_rotation.setEulerZYX(0.0, 0.0, q[segment]);
+      pose *= fcl::Transform3f(joint_angle_rotation);
+    }
+    parts_[part]->setTransform(pose);
+
+    fcl::DistanceRequest request(false, 1.0, variance_);
+    fcl::DistanceResult result;
+    fcl::distance(parts_[part].get(), environment_.get(), request, result);
+
+    distances.emplace_back(
+        std::max(result.min_distance, 0.0), std::vector<double>(0));
+
+    std::vector<double>* radiuses = &(distances.back().second);
+    p = pose.transform(cylinders_[part].first);
+    for (int seg = 0; seg <= segment; ++seg) {
+      radiuses->push_back(cylinders_[part].second + std::max(
+          PointDistanceToAxis(p - ax_P[seg], ax_O[seg]),
+          PointDistanceToAxis(p_prev - ax_P[seg], ax_O[seg])));
+    }
+    p_prev = p;
+  }
+
+  return distances;
+}
+
+void FclEnvironment::LoadDh(
+    const std::vector<std::vector<double> >& configuration) {
+  // each part gets transformed ignoring the last coordinate transform
+  dh_inverted_.emplace_back(new fcl::Transform3f);
+  for (const std::vector<double>& param : configuration) {
+    dh_parameters_.emplace_back(new fcl::Transform3f);
+    fcl::Transform3f* t = dh_parameters_.back().get();
+
+    fcl::Matrix3f rotations;
+    rotations.setEulerZYX(0.0, 0.0, param[1]);
+    *t *= fcl::Transform3f(rotations);
+    *t *= fcl::Transform3f(fcl::Vec3f(param[0], 0.0, param[2]));
+    rotations.setEulerZYX(param[3], 0.0, 0.0);
+    *t *= fcl::Transform3f(rotations);
+
+    dh_inverted_.emplace_back(new fcl::Transform3f(*t));
+    fcl::Transform3f* t_inv = dh_inverted_.back().get();
+    t_inv->inverse();
+    *t_inv *= *dh_inverted_[dh_inverted_.size() - 2];
+  }
+}
+
+void FclEnvironment::ConfigureFromPtree(
+    const boost::property_tree::ptree& config_tree,
+    const boost::filesystem::path& config_file_path) {
+  std::string robot = config_tree.get<std::string>("robot");
+  boost::filesystem::path robot_file_path(config_file_path / robot);
+  boost::property_tree::ptree robot_tree;
+  boost::property_tree::read_json(robot_file_path.native(), robot_tree);
+  robot_file_path.remove_filename();
+
+  std::vector<std::vector<double> > config;
+  for (auto& item : robot_tree.get_child("dh")) {
+    config.emplace_back();
+    for (auto& param : item.second)
+      config.back().push_back(param.second.get_value<double>());
+  }
+
+  std::vector<int> parts_per_joint;
+  for (auto& item : robot_tree.get_child("parts_per_joint"))
+    parts_per_joint.push_back(item.second.get_value<int>());
+  std::vector<std::string> parts;
+  for (auto& item : robot_tree.get_child("parts"))
+    parts.push_back(
+        (robot_file_path / item.second.get_value<std::string>()).native());
+  std::string environment = config_tree.get<std::string>("environment");
+  double max_underestimate = config_tree.get<double>("max_underestimate");
+
+  part_count_ = parts.size();
+  variance_ = max_underestimate;
+  is_joint_start_.resize(parts.size(), false);
+
+  LoadDh(config);
+
+  int part_index = 0;
+  int segment_index = -1;
+  is_joint_start_[0] = true;
+  for (int parts_c : parts_per_joint) {
+    part_index += parts_c;
+    if (part_index < part_count_)
+      is_joint_start_[part_index] = true;
+  }
+  part_index = 0;
+  for (const std::string& part : parts) {
+    if (is_joint_start_[part_index])
+      ++segment_index;
+    double l, r;
+    fcl::Vec3f v = dh_parameters_[segment_index]->getTranslation();
+    v.normalize();
+    double x = v[0];
+    double y = v[1];
+    double z = v[2];
+    part_meshes_.emplace_back(ParseModel(
+        part, dh_inverted_[segment_index].get(), x, y, z, &l, &r));
+    parts_.emplace_back(new fcl::CollisionObject(
+        part_meshes_.back(), fcl::Transform3f()));
+    cylinders_.emplace_back(fcl::Vec3f(x * l, y * l, z * l), r);
+    part_index++;
+  }
+  double l, r;
+  environment_mesh_.reset(ParseModel(
+      (config_file_path / environment).native(), dh_inverted_[0].get(),
+      0.0, 0.0, 0.0, &l, &r));
+  environment_.reset(new fcl::CollisionObject(
+      environment_mesh_, fcl::Transform3f()));
+}
+
+}  // namespace environment
+}  // namespace bubblesmp
+}  // namespace ademovic
+}  // namespace com
---
+++ src/environment/fcl-environment_test.cc
@@ -0,0 +1,360 @@
+//
+// Copyright (c) 2015, Adnan Ademovic
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE FclEnvironment
+#include "fcl-environment.h"
+#include <boost/test/unit_test.hpp>
+#include <cstdio>
+
+using ::com::ademovic::bubblesmp::environment::EnvironmentInterface;
+using ::com::ademovic::bubblesmp::environment::FclEnvironment;
+
+void MakeFile(const char filename[], const char input[]) {
+  FILE* f = fopen(filename, "w");
+  fprintf(f, "%s", input);
+  fclose(f);
+}
+
+void MakeModelFile(const char filename[], const std::vector<float>& coords) {
+  FILE* f = fopen(filename, "wb");
+  uint8_t header[80];
+  float normal[] = {0.0f, 0.0f, 0.0f};
+  uint16_t attribute = 0;
+  uint32_t triangle_count = static_cast<uint32_t>(coords.size() / 9);
+  fwrite(header, sizeof(header[0]), 80, f);
+  fwrite(&triangle_count, sizeof(triangle_count), 1, f);
+  for (size_t i = 0; i < coords.size(); i += 9) {
+    fwrite(normal, sizeof(normal[0]), 3, f);
+    for (size_t j = 0; j < 9; ++j)
+      fwrite(&coords[j], sizeof(coords[0]), 1, f);
+    fwrite(&attribute, sizeof(attribute), 1, f);
+  }
+  fclose(f);
+}
+
+double AbsToRelTolerance(double value, double tolerance) {
+  return 100.0 * tolerance / value;
+}
+
+BOOST_AUTO_TEST_CASE(trivial_collision) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg1.testfile\"],"
+      "\"parts_per_joint\": [1],"
+      "\"dh\": [[10, 0, 0, 0]]"
+      "}");
+  MakeModelFile("seg1.testfile", {10, 0, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("env.testfile", {5, -10, -10, 5, -10, 10, 5, 10, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({1.57}), false);
+}
+
+BOOST_AUTO_TEST_CASE(two_segment_collision) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg1.testfile\", \"seg2.testfile\"],"
+      "\"parts_per_joint\": [1, 1],"
+      "\"dh\": [[10, 0, 0, 0], [10, 0, 0, 0]]"
+      "}");
+  MakeModelFile("seg1.testfile", {10, 0, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("seg2.testfile", {20, 0, 0, 10, 0, 1, 10, 0, -1});
+  MakeModelFile("env.testfile", {8, -10, -10, 8, -10, 10, 8, 10, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.0, 0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.78, 0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.78, 0.78}), false);
+}
+
+BOOST_AUTO_TEST_CASE(three_segment_collision) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg1.testfile\", \"seg2.testfile\", \"seg3.testfile\"],"
+      "\"parts_per_joint\": [1, 1, 1],"
+      "\"dh\": [[10, 0, 0, 0], [10, 0, 0, 0], [5, 0, 0, 0]]"
+      "}");
+  MakeModelFile("seg1.testfile", {10, 0, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("seg2.testfile", {20, 0, 0, 10, 0, 1, 10, 0, -1});
+  MakeModelFile("seg3.testfile", {25, 0, 0, 20, 0, 1, 20, 0, -1});
+  MakeModelFile("env.testfile", {8, -20, -10, 8, -20, 10, 8, 20, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.0, 0.0, 0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.78, 0.0, 0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.78, 0.78, 0.0}), false);
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.78, 0.78, -1.57}), true);
+}
+
+BOOST_AUTO_TEST_CASE(three_segment_transformation_stacking) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg1.testfile\", \"seg2.testfile\", \"seg3.testfile\"],"
+      "\"parts_per_joint\": [1, 1, 1],"
+      "\"dh\": [[10, 1.57, 0, 0], [10, 0, 0, 0], [5, 0, 0, 0]]"
+      "}");
+  MakeModelFile("seg1.testfile", {0, 10, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("seg2.testfile", {0, 20, 0, 0, 10, 1, 0, 10, -1});
+  MakeModelFile("seg3.testfile", {0, 25, 0, 0, 20, 1, 0, 20, -1});
+  MakeModelFile("env.testfile", {8, -20, -10, 8, -20, 10, 8, 20, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  BOOST_CHECK_EQUAL(environment.IsCollision({-1.57, 0.0, 0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({-0.78, 0.0, 0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({-0.78, 0.78, 0.0}), false);
+  BOOST_CHECK_EQUAL(environment.IsCollision({-0.78, 0.78, -1.57}), true);
+}
+
+BOOST_AUTO_TEST_CASE(three_segment_multiple_part_collision) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg11.testfile\", \"seg12.testfile\", "
+                  "\"seg21.testfile\", \"seg22.testfile\", "
+                  "\"seg31.testfile\", \"seg32.testfile\"],"
+      "\"parts_per_joint\": [2, 2, 2],"
+      "\"dh\": [[10, 0, 0, 0], [10, 0, 0, 0], [5, 0, 0, 0]]"
+      "}");
+  MakeModelFile("seg11.testfile", {5, 0, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("seg12.testfile", {10, 0, 0, 5, 0, 1, 5, 0, -1});
+  MakeModelFile("seg21.testfile", {15, 0, 0, 10, 0, 1, 10, 0, -1});
+  MakeModelFile("seg22.testfile", {20, 0, 0, 15, 0, 1, 15, 0, -1});
+  MakeModelFile("seg31.testfile", {22.5, 0, 0, 20, 0, 1, 20, 0, -1});
+  MakeModelFile("seg32.testfile", {25, 0, 0, 22.5, 0, 1, 22.5, 0, -1});
+  MakeModelFile("env.testfile", {8, -20, -10, 8, -20, 10, 8, 20, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.0, 0.0, 0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.78, 0.0, 0.0}), true);
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.78, 0.78, 0.0}), false);
+  BOOST_CHECK_EQUAL(environment.IsCollision({0.78, 0.78, -1.57}), true);
+}
+
+BOOST_AUTO_TEST_CASE(trivial_distance) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg1.testfile\"],"
+      "\"parts_per_joint\": [1],"
+      "\"dh\": [[10, 0, 0, 0]]"
+      "}");
+  MakeModelFile("seg1.testfile", {10, 0, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("env.testfile", {15, -10, -10, 15, -10, 10, 15, 10, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  EnvironmentInterface::DistanceProfile d;
+
+  d = environment.GetDistanceProfile({0.0});
+  BOOST_CHECK_EQUAL(d.size(), 1);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 5.0, AbsToRelTolerance(5.0, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 11.0, 1.0);
+
+  d = environment.GetDistanceProfile({0.78});
+  BOOST_CHECK_EQUAL(d.size(), 1);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 7.89, AbsToRelTolerance(7.89, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 11.0, 1.0);
+}
+
+BOOST_AUTO_TEST_CASE(trivial_distance_two_part) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg11.testfile\", \"seg12.testfile\"],"
+      "\"parts_per_joint\": [2],"
+      "\"dh\": [[10, 0, 0, 0]]"
+      "}");
+  MakeModelFile("seg11.testfile", {5, 0, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("seg12.testfile", {10, 0, 0, 5, 0, 1, 5, 0, -1});
+  MakeModelFile("env.testfile", {15, -10, -10, 15, -10, 10, 15, 10, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  EnvironmentInterface::DistanceProfile d;
+
+  d = environment.GetDistanceProfile({0.0});
+  BOOST_CHECK_EQUAL(d.size(), 2);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_EQUAL(d[1].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 10.0, AbsToRelTolerance(10.0, 0.11));
+  BOOST_CHECK_CLOSE(d[1].first, 5.0, AbsToRelTolerance(5.0, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 6.0, 1.0);
+  BOOST_CHECK_CLOSE(d[1].second[0], 11.0, 1.0);
+
+  d = environment.GetDistanceProfile({0.78});
+  BOOST_CHECK_EQUAL(d.size(), 2);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_EQUAL(d[1].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 11.4454, AbsToRelTolerance(11.4454, 0.11));
+  BOOST_CHECK_CLOSE(d[1].first, 7.89, AbsToRelTolerance(7.89, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 6.0, 1.0);
+  BOOST_CHECK_CLOSE(d[1].second[0], 11.0, 1.0);
+}
+
+BOOST_AUTO_TEST_CASE(two_segment_distance) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg1.testfile\", \"seg2.testfile\"],"
+      "\"parts_per_joint\": [1, 1],"
+      "\"dh\": [[10, 0, 0, 0], [10, 0, 0, 0]]"
+      "}");
+  MakeModelFile("seg1.testfile", {10, 0, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("seg2.testfile", {20, 0, 0, 10, 0, 1, 10, 0, -1});
+  MakeModelFile("env.testfile", {25, -20, -10, 25, -20, 10, 25, 20, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  EnvironmentInterface::DistanceProfile d;
+
+  d = environment.GetDistanceProfile({0.0, 0.0});
+  BOOST_CHECK_EQUAL(d.size(), 2);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 15.0, AbsToRelTolerance(15.0, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 11.0, 1.0);
+  BOOST_CHECK_EQUAL(d[1].second.size(), 2);
+  BOOST_CHECK_CLOSE(d[1].first, 5.0, AbsToRelTolerance(5.0, 0.11));
+  BOOST_CHECK_CLOSE(d[1].second[0], 21.0, 1.0);
+  BOOST_CHECK_CLOSE(d[1].second[1], 11.0, 1.0);
+
+  d = environment.GetDistanceProfile({0.78, 0.0});
+  BOOST_CHECK_EQUAL(d.size(), 2);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 17.89, AbsToRelTolerance(17.89, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 11.0, 1.0);
+  BOOST_CHECK_EQUAL(d[1].second.size(), 2);
+  BOOST_CHECK_CLOSE(d[1].first, 10.78, AbsToRelTolerance(10.78, 0.11));
+  BOOST_CHECK_CLOSE(d[1].second[0], 21.0, 1.0);
+  BOOST_CHECK_CLOSE(d[1].second[1], 11.0, 1.0);
+
+  d = environment.GetDistanceProfile({0.78, 0.78});
+  BOOST_CHECK_EQUAL(d.size(), 2);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 17.89, AbsToRelTolerance(17.89, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 11.0, 1.0);
+  BOOST_CHECK_EQUAL(d[1].second.size(), 2);
+  BOOST_CHECK_CLOSE(d[1].first, 17.78, AbsToRelTolerance(17.78, 0.11));
+  BOOST_CHECK_CLOSE(d[1].second[0], 19.498, 1.0);
+  BOOST_CHECK_CLOSE(d[1].second[1], 11.0, 1.0);
+
+  d = environment.GetDistanceProfile({0.0, 2.5});
+  BOOST_CHECK_EQUAL(d.size(), 2);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 15.0, AbsToRelTolerance(15.0, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 11.0, 1.0);
+  BOOST_CHECK_EQUAL(d[1].second.size(), 2);
+  BOOST_CHECK_CLOSE(d[1].first, 15.0, AbsToRelTolerance(5.0, 0.11));
+  BOOST_CHECK_CLOSE(d[1].second[0], 11.0, 1.0);
+  BOOST_CHECK_CLOSE(d[1].second[1], 11.0, 1.0);
+}
+
+BOOST_AUTO_TEST_CASE(three_segment_3d_distance) {
+  MakeFile("conf.testfile", "{"
+      "\"robot\": \"robot.testfile\","
+      "\"environment\": \"env.testfile\","
+      "\"max_underestimate\": 0.1"
+      "}");
+  MakeFile("robot.testfile", "{"
+      "\"parts\": [\"seg1.testfile\", \"seg2.testfile\", \"seg3.testfile\"],"
+      "\"parts_per_joint\": [1, 1, 1],"
+      "\"dh\": [[10, 0, 0, 0], [10, 0, 0, 1.57], [10, 1.57, 0, 0]]"
+      "}");
+  MakeModelFile("seg1.testfile", {10, 0, 0, 0, 0, 1, 0, 0, -1});
+  MakeModelFile("seg2.testfile", {20, 0, 0, 10, 0, 1, 10, 0, -1});
+  MakeModelFile("seg3.testfile", {20, 0, 10, 20, 1, 0, 20, -1, 0});
+  MakeModelFile("env.testfile", {35, -20, -10, 35, -20, 10, 35, 20, 0});
+
+  FclEnvironment environment("conf.testfile");
+
+  EnvironmentInterface::DistanceProfile d;
+
+  d = environment.GetDistanceProfile({0.0, 0.0, 0.0});
+  BOOST_CHECK_EQUAL(d.size(), 3);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 25.0, AbsToRelTolerance(25.0, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 11.0, 1.0);
+  BOOST_CHECK_EQUAL(d[1].second.size(), 2);
+  BOOST_CHECK_CLOSE(d[1].first, 15.0, AbsToRelTolerance(15.0, 0.11));
+  BOOST_CHECK_CLOSE(d[1].second[0], 21.0, 1.0);
+  BOOST_CHECK_CLOSE(d[1].second[1], 11.0, 1.0);
+  BOOST_CHECK_EQUAL(d[2].second.size(), 3);
+  BOOST_CHECK_CLOSE(d[2].first, 15.0, AbsToRelTolerance(15.0, 0.11));
+  BOOST_CHECK_CLOSE(d[2].second[0], 21.0, 1.0);
+  BOOST_CHECK_CLOSE(d[2].second[1], 11.0, 1.0);
+  BOOST_CHECK_CLOSE(d[2].second[2], 11.0, 1.0);
+
+  d = environment.GetDistanceProfile({0.0, 0.0, -1.57});
+  BOOST_CHECK_EQUAL(d.size(), 3);
+  BOOST_CHECK_EQUAL(d[0].second.size(), 1);
+  BOOST_CHECK_CLOSE(d[0].first, 25.0, AbsToRelTolerance(25.0, 0.11));
+  BOOST_CHECK_CLOSE(d[0].second[0], 11.0, 1.0);
+  BOOST_CHECK_EQUAL(d[1].second.size(), 2);
+  BOOST_CHECK_CLOSE(d[1].first, 15.0, AbsToRelTolerance(15.0, 0.11));
+  BOOST_CHECK_CLOSE(d[1].second[0], 21.0, 1.0);
+  BOOST_CHECK_CLOSE(d[1].second[1], 11.0, 1.0);
+  BOOST_CHECK_EQUAL(d[2].second.size(), 3);
+  BOOST_CHECK_CLOSE(d[2].first, 5.0, AbsToRelTolerance(5.0, 0.11));
+  BOOST_CHECK_CLOSE(d[2].second[0], 31.0, 1.0);
+  BOOST_CHECK_CLOSE(d[2].second[1], 21.0, 1.0);
+  BOOST_CHECK_CLOSE(d[2].second[2], 11.0, 1.0);
+}
